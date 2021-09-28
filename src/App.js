2ac406ae257dacd2ba98d71d4eafb154097d6a83
mayur-argade <argademayur2002@gmail.com>
2021-09-28 20:33:59 +0530

    Project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lakshya</h1>
+      <h2>Your online learning friend</h2>
+    </div>
+  );
+}
+
+export default App;
