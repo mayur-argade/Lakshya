eeaf7dde4d6bf4b2584c088e6af84dce071f648b
SumedhBengale <ssbengale@gmail.com>
2021-10-21 13:46:06 +0530

    Final Preview of Profile Photo Page. SVG removed from 2nd Page.

--- src/pages/personal-info-page/personal.jsx
+++ src/pages/personal-info-page/personal.jsx
@@ -1,7 +1,9 @@
-import React,{useState,useRef,useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import './personal.css';
 import { Container, CssBaseline, Typography, Grid, Button, TextField } from '@mui/material';
-import svg1 from './svg_art.svg';
+import svg1 from '../../images/svg_art.svg';
+import placeholder from '../../images/profilePlaceholder.png';
+
 const PersonalInfoPage = () => {
     const [page, setPage]=useState(1);
     
@@ -58,16 +60,16 @@ const PersonalInfoPage = () => {
                     <Grid item align='center' xs={12} md={6} xl={6}>
                         <img src={svg1} style={{marginLeft:30}} alt="SVG"></img>
                     </Grid>
-                    <Grid item xs={12} md={6} xl={6}>
+                    <Grid item align='center' xs={12} md={6} xl={6}>
                         <Container style={{marginTop:30}}>
                             <Grid container spacing='30'>
                                 <Grid item xs={6} md={6}>
-                                    <TextField fullWidth label="First Name" onChange={getFirstName} InputLabelProps={{ shrink: true }}placeholder='First Name' variant="outlined" />
+                                    <TextField fullWidth label="First Name" onChange={getFirstName} InputLabelProps={{ shrink: true }}placeholder='First Name' variant="outlined" borderColor='#A405DC'/>
                                 </Grid>
 
 
                                 <Grid item xs={6} md={6}>
-                                    <TextField fullWidth label="Last Name" onChange={getLastName} InputLabelProps={{ shrink: true }} placeholder='Last Name'variant="outlined" />
+                                    <TextField fullWidth label="Last Name" onChange={getLastName} InputLabelProps={{ shrink: true}} placeholder='Last Name'variant="outlined" />
                                 </Grid> 
 
 
@@ -87,7 +89,7 @@ const PersonalInfoPage = () => {
 
 
                                 <Grid item xs={12} md={12} xl={12} align='center'>
-                                    <Button variant='contained' size='large' onClick={nextPage} style={{backgroundColor: '#A405DC',marginBottom:10 ,color: '#FFFFFF', borderRadius: "0.7em"}}>Next</Button>
+                                    <Button variant='contained' size='large' onClick={nextPage} style={{marginBottom:10, borderRadius: '0.7em',color:'#FFFFFF',backgroundColor:'#A405DC'}}>Next</Button>
                                 </Grid>
                             </Grid>
                         </Container>
@@ -102,81 +104,65 @@ const PersonalInfoPage = () => {
         function prevPage(){
             setPage(page=>page-1);
         }
-        const fileInputRef=useRef(null);
-        const [preview, setPreview]=useState(null);
         const [image, setImage]=useState(null);
+        
+        const [{alt, src}, setImg] = useState({
+            src: placeholder,
+            alt: 'Upload an Image'
+        });
+    
+        const handleImg = (e) => {
+            if(e.target.files[0]) {
+                setImg({
+                    src: URL.createObjectURL(e.target.files[0]),
+                    alt: e.target.files[0].name
+                });    
+            }   
+        }
 
         useEffect(()=>{
             if(image){
                 const reader =new FileReader();
                 reader.onloadend=()=>{
-                    setPreview(reader.result);
+                    setImage(reader.result);
                 }
                 reader.readAsDataURL(image);
             }
             else{
-                setPreview(null);
+                setImage(null);
             }
         },[image]);
         return (
             <>
             <CssBaseline/>
             <Container maxWidth='md'>
-
                 <Typography align='center' variant='h2' style={{color:'#A405DC',fontWeight:'600',marginTop:100}}>Lakshya</Typography>
                 <Typography variant='h2' fontWeight='400' align='center'>Finish your Account Setup</Typography>
                 <Typography variant='h6' fontWeight='400' align='center'>We will never share your personal information</Typography>
-            
             </Container>
-            <Container>
-                <Grid container>
-                    <Grid item align='center' xs={12} md={6} xl={6}>
-                        <img src={svg1} style={{marginLeft:30}} alt="SVG"></img>
+                <Grid container style={{marginTop:30, spacing:30,}}>
+                    <Grid item xs={12} md={12} xl={12} style={{marginBottom:30,align:'center'}}>
+                        <div className="form__img-input-container">
+                            <input 
+                                type="file" 
+                                accept=".png, .jpg, .jpeg" 
+                                id="photo" 
+                                className="visually-hidden"
+                                onChange={handleImg}
+                            />
+                            <label htmlFor="photo" className="form-img__file-label"/>
+                            <img src={src} alt={alt} className="form-img__img-preview"/>
+                        </div>
                     </Grid>
-                    <Grid item xs={12} md={6} xl={6}>
-                        <Container style={{marginTop:30}}>
-                            <Grid container spacing='30'>
-                                <Grid item xs={12} md={12} xl={12} align='center'>
-                                    <Container>
-                                        {preview?<img src={preview}></img>:
-                                            <Button onClick={(event)=>{
-                                                    event.preventDefault();
-                                                    fileInputRef.current.click();
-                                                    }}>Add Profile Photo
-                                                </Button>}
-                                            <input
-                                                type="file"
-                                                style={{display:'none'}}
-                                                ref={fileInputRef}
-                                                accept="image/*"
-                                                onChange={(event)=>{
-                                                    const file=event.target.files[0];
-                                                    if(file){
-                                                        setImage(file);
-                                                    }
-                                                    else{
-                                                        setImage(null);
-                                                    }
-                                                }}
-                                                >
-                                            </input>
-                                     </Container>
-                                </Grid>
+                    <Grid item xs={6} md={6} xl={6} align='right' style={{paddingRight:60}}>
+                        <Button variant='outlined' size='large' onClick={prevPage} style={{marginBottom:10 ,color:'#F44336', borderRadius:'0.7em',borderColor:'#F44336'}}>Back</Button>
+                    </Grid>
 
 
-                                <Grid item xs={6} md={6} xl={6} align='center'>
-                                    <Button variant='contained' size='large' onClick={prevPage} style={{backgroundColor: '#F44336',marginBottom:10 ,color: '#FFFFFF', borderRadius: "0.7em"}}>Back</Button>
-                                </Grid>
-
-
-                                <Grid item xs={6} md={6} xl={6} align='center'>
-                                    <Button variant='contained' size='large' style={{backgroundColor: '#A405DC',marginBottom:10 ,color: '#FFFFFF', borderRadius: "0.7em"}}>Submit</Button>
-                                </Grid>
-                            </Grid>
-                        </Container>
+                    <Grid item xs={6} md={6} xl={6} align='left' style={{paddingLeft:60}}>
+                        <Button variant='contained' size='large' style={{marginBottom:10, borderRadius: '0.7em',color:'#FFFFFF',backgroundColor:'#A405DC'}}>Submit</Button>
                     </Grid>
                 </Grid>
-            </Container>
             </>
         )
     }
